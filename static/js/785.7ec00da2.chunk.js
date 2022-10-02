6a36ce2015143b374c5ba5e88991c23cb83b256b
IhorKozloff <87847488+IhorKozloff@users.noreply.github.com>
2022-10-02 13:57:58 +0000

    Deploying to gh-pages from @ IhorKozloff/dragon-reviewer@c94b9cbfe64438047c9523c1b44b67d8464c9f92 🚀

--- static/js/785.7ec00da2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdragon_reviewer=self.webpackChunkdragon_reviewer||[]).push([[785],{5785:function(e,r,n){n.r(r),n.d(r,{default:function(){return i}});var s=n(1480),a=n(1560),c=n(184);function i(){return(0,c.jsx)(s.$0,{className:"favorites",children:(0,c.jsx)(s.W2,{className:"favorites__container",children:(0,c.jsx)(a.lL,{})})})}}}]);
-//# sourceMappingURL=785.7ec00da2.chunk.js.map
